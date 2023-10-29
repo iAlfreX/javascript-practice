13c1e0dd1511a5230057905156b77263e7e0bc4c
Anton Sharenko <alpha.tosha@gmail.com>
2023-10-29 17:35:07 +0200

    29.10.23 homework-1

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,57 @@
-const array = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
-const sumOfElements = array.reduce((accumulator, el) => accumulator + el, 0);
+const array = createArray();
 
-const sumOfPairedPositiveElements = array
-  .filter((el) => el > 0 && el % 2 === 0)
-  .reduce((accumulator, el) => accumulator + el, 0);
+outputArray(array);
 
-const sumOfUnpairedPositiveElements = array
-  .filter((el) => el > 0 && el % 2 !== 0)
-  .reduce((accumulator, el) => accumulator + el, 0);
+if (typeof array !== "undefined") {
+  const item = prompt("Введите элемент массива, который нужно удалить: ");
 
-const numberOfPositiveElements = array.filter((el) => el > 0).length,
-  numberOfNegativeElements = array.filter((el) => el < 0).length;
+  if (!enableNullCheck(item)) {
+    removeElement(array, item);
+    outputArray(array);
+  }
+}
 
-const maxEl = array.reduce((accumulator, el) => {
-  if (el > accumulator) return el;
+function removeElement(array, item) {
+  if (Array.isArray(array) && !isNaN(item)) {
+    if (array.length !== 0) {
+      const index = array.findIndex((el) => el === +item);
 
-  return accumulator;
-}, array[0]);
+      index !== -1 ? array.splice(index, 1) : alert("Данного элемента нет в массиве!");
+    } else {
+      alert("Массив пустой!");
+    }
+  } else {
+    alert("Входные данные неверны!");
+  }
+}
 
-const minEl = array.reduce((accumulator, el) => {
-  if (el < accumulator) return el;
+function createArray() {
+  const array = [];
+  let input = prompt("Введите числовые элементы массива или 'готово', чтобы закончить:");
 
-  return accumulator;
-}, array[0]);
+  if (enableNullCheck(input)) return;
 
-const maxElIndex = array.indexOf(maxEl),
-  minElIndex = array.indexOf(minEl); // Не зрозумів щодо порядкового номера! Якщо це не індекс то треба при виводі константи додати +1.
+  while (input.toLowerCase() !== "готово") {
+    if (!isNaN(input)) {
+      array.push(+input);
+    } else {
+      alert("Вы ввели не число! Попробуйте еще раз!");
+    }
 
-const numberOfUnpairedPositiveElements = array.filter((el) => el > 0 && el % 2 !== 0).length,
-  numberOfPairedPositiveElements = array.filter((el) => el > 0 && el % 2 === 0).length;
+    input = prompt("Введите следующий элемент массива или 'готово', чтобы закончить:");
 
-const productOfPositiveElements = array.filter((el) => el > 0).reduce((accumulator, el) => accumulator * el, 1);
-const arrayWithOneLargestValue = array.map((el) => {
-  if (el !== maxEl) el = 0;
+    if (enableNullCheck(input)) return;
+  }
 
-  return el;
-});
+  return array;
+}
 
-console.log(`Вихідний масив: ${array.join(", ")}.`);
-console.log(``);
-console.log(`Сума всіх елементів: ${sumOfElements}. Кількість його позитивних елементів: ${numberOfPositiveElements}.`);
-console.log(`Максимальний елемент масива: ${maxEl}. Його індекс: ${maxElIndex}.`);
-console.log(`Мінімальний елемент масива: ${minEl}. Його індекс: ${minElIndex}.`);
-console.log(`Кількість негативних елементів: ${numberOfNegativeElements}.`);
-console.log(`Кількість непарних позитивних елементів: ${numberOfUnpairedPositiveElements}.`);
-console.log(`Кількість парних позитивних елементів: ${numberOfPairedPositiveElements}.`);
-console.log(`Сума парних позитивних елементів: ${sumOfPairedPositiveElements}.`);
-console.log(`Сума непарних позитивних елементів: ${sumOfUnpairedPositiveElements}.`);
-console.log(`Добуток парних позитивних елементів: ${productOfPositiveElements}.`);
-console.log(`Масив з обнуленими значеннями окрім найбільшого значення: ${arrayWithOneLargestValue.join(", ")}.`);
+function outputArray(array) {
+  if (Array.isArray(array)) {
+    alert(`Элементы массива: ${array}.`);
+  }
+}
+
+function enableNullCheck(input) {
+  if (input === null) return true;
+}
