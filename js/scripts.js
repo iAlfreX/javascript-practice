f7700a7dc35884c077a49dfaeaa40bdf35524451
Anton Sharenko <alpha.tosha@gmail.com>
2023-11-13 02:20:48 +0200

    homework-1 13.11.2023

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,49 @@
-const checkForNull = (value) => (value === null ? true : false);
-const checkForNaN = (value) => (isNaN(value) ? true : false);
-const checkForNumber = (value) => (typeof value === "number" ? true : false);
-
-const sum = createSumFunction();
-
-if (typeof sum === "function") {
-  output(sum(3));
-  output(sum(5));
-  output(sum(20));
-  output(sum("hello"));
-  output(sum(5));
-  output(sum(3, true));
-  output(sum(5));
-  output(sum(20));
+class Human {
+  constructor(name, gender) {
+    this.name = name;
+    this.gender = gender;
+  }
 }
 
-function createSumFunction() {
-  const tempSum = prompt("Введите начальное значение-состояние к которому будет необходимо прибавлять числа: ", "0");
+class Apartment {
+  residents = [];
 
-  if (checkForNull(tempSum)) return;
+  addResident(resident) {
+    if (resident instanceof Human) this.residents.push(resident);
+    else console.log("Переданный аргумент не является человеком!");
+  }
+}
 
-  let sum = Number(tempSum);
-
-  if (checkForNaN(sum) || !checkForNumber(sum)) {
-    alert("Ошибка! Вы ввели не число!");
-    return;
+class House {
+  constructor(maxNumOfApartments) {
+    this.maxNumOfApartments = maxNumOfApartments;
   }
 
-  return function (value, reset = false) {
-    if (checkForNaN(value) || !checkForNumber(value)) return;
+  apartments = [];
 
-    if (reset === true) sum = 0;
-
-    return (sum += value);
-  };
+  addApartment(apartment) {
+    if (apartment instanceof Apartment)
+      this.apartments.length < this.maxNumOfApartments
+        ? this.apartments.push(apartment)
+        : console.log(`Невозможно добавить квартиру! Максимальное количество квартир в доме ${this.maxNumOfApartments}.`);
+    else console.log("Переданный аргумент не является квартирой!");
+  }
 }
 
-function output(value) {
-  alert(value);
-}
+const john = new Human("John", "Male");
+const jane = new Human("Jane", "Female");
+const bob = new Human("Bob", "Male");
+const alice = new Human("Alice", "Female");
+
+const firstApartment = new Apartment();
+const secondApartment = new Apartment();
+firstApartment.addResident(john);
+firstApartment.addResident(jane);
+secondApartment.addResident(bob);
+secondApartment.addResident(alice);
+
+const myHouse = new House(2);
+myHouse.addApartment(firstApartment);
+myHouse.addApartment(secondApartment);
+
+console.log(myHouse);
