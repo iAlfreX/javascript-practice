41f49b9cd51f050c5e9e5876c7ac9cffcf2da4f3
Anton Sharenko <alpha.tosha@gmail.com>
2023-11-30 01:21:51 +0200

    homework-1 30.11.2023

--- js/scripts.js
+++ js/scripts.js
@@ -1,125 +1,68 @@
-class DataValidator {
-  static isTrueNumber(value) {
-    return typeof value === "number" && !isNaN(value) && isFinite(value) && Number.isInteger(value);
+"use strict";
+
+class DataValidation {
+  static checkForNumber(value) {
+    return typeof value === "number" && !isNaN(value);
   }
 
-  static isTrueYear(value) {
-    return value >= 1990 && value <= new Date().getFullYear();
-  }
-
-  static isTrueArray(value) {
-    return value instanceof Array;
-  }
-
-  static isTrueString(value) {
-    return typeof value === "string" && value.trim() !== "";
+  static checkForInteger(value) {
+    return Number.isInteger(value);
   }
 }
 
-class Student {
-  constructor(name, surname, yearOfBirth, points) {
-    this.name = name;
-    this.surname = surname;
-    this.yearOfBirth = yearOfBirth;
-    this.points = points;
-    this.attendance = [];
+class Table {
+  constructor(rows, cols) {
+    this.rows = DataValidation.checkForNumber(rows) && DataValidation.checkForInteger(rows) ? rows : 0;
+    this.cols = DataValidation.checkForNumber(cols) && DataValidation.checkForInteger(cols) ? cols : 0;
   }
 
-  get fullName() {
-    return DataValidator.isTrueString(this.name) && DataValidator.isTrueString(this.surname)
-      ? this.name + " " + this.surname
-      : "--нет данных--";
+  get amountOfRows() {
+    return this.rows;
   }
 
-  get age() {
-    if (DataValidator.isTrueNumber(this.yearOfBirth) && DataValidator.isTrueYear(this.yearOfBirth)) {
-      const currentDate = new Date();
-      const birthDate = new Date(this.yearOfBirth, 0, 1);
-      const ageInMilliseconds = currentDate - birthDate;
-      const ageInYears = Math.floor(ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25));
-
-      return ageInYears;
+  set amountOfRows(value) {
+    if (DataValidation.checkForNumber(value) && DataValidation.checkForInteger(value)) {
+      this.rows = value;
+    } else {
+      console.log(`${value} не является допустимым значением!`);
     }
-
-    return "--нет данных--";
   }
 
-  get averageScore() {
-    if (DataValidator.isTrueArray(this.points)) {
-      const averageScore =
-        this.points.reduce((sum, point) => {
-          const isTruePoint = point >= 1 && point <= 100;
+  get amountOfCols() {
+    return this.cols;
+  }
 
-          if (DataValidator.isTrueNumber(point) && isTruePoint) return (sum += point);
-        }, 0) / this.points.length;
-
-      return Math.round(averageScore);
+  set amountOfCols(value) {
+    if (DataValidation.checkForNumber(value) && DataValidation.checkForInteger(value)) {
+      this.cols = value;
+    } else {
+      console.log(`${value} не является допустимым значением!`);
     }
-
-    return "--нет данных--";
   }
 
-  fillAttendance() {
-    if (DataValidator.isTrueArray(this.attendance)) {
-      while (this.attendance.length < 25) {
-        const visit = Math.round(Math.random());
+  createTable() {
+    const table = document.createElement("table");
+    let counter = 0;
+    table.style.cssText = "border-collapse: collapse; width: 50%; height: 50%;";
+    // table.style.borderCollapse = "collapse";
+    // table.style.width = "100%";
+    // table.style.border = "1px solid #000";
+    document.body.style.cssText = "margin: 0; display: flex; justify-content: center; align-items: center; height: 100vh;";
+    document.body.insertBefore(table, document.body.firstChild);
 
-        visit ? this.present() : this.absent();
+    for (let rows = 1; rows <= this.amountOfRows; rows++) {
+      const tableRow = document.createElement("tr");
+      table.appendChild(tableRow);
+
+      for (let cols = 1; cols <= this.amountOfCols; cols++) {
+        const tableData = document.createElement("td");
+        tableData.style.cssText = "text-align: center; width: 10%; border: 1px solid #000;";
+        tableData.textContent = ++counter;
+        tableRow.appendChild(tableData);
       }
     }
-  }
-
-  present() {
-    if (this.attendance.length < 25) {
-      this.attendance.push(true);
-    }
-  }
-
-  absent() {
-    if (this.attendance.length < 25) {
-      this.attendance.push(false);
-    }
-  }
-
-  get summary() {
-    if (DataValidator.isTrueArray(this.attendance)) {
-      const averageAttendance = this.attendance.filter((attendance) => attendance === true).length / this.attendance.length;
-
-      let resultString = "";
-
-      switch (true) {
-        case this.averageScore >= 90 && averageAttendance >= 0.9:
-          resultString += "Молодец!";
-          break;
-        case (this.averageScore < 90 && averageAttendance >= 0.9) || (this.averageScore >= 90 && averageAttendance < 0.9):
-          resultString += "Хорошо, но можно лучше!";
-          break;
-        default:
-          resultString += "Редиска!";
-      }
-
-      return resultString;
-    }
-
-    return "--нет данных--";
-  }
-
-  get consoleFullInfo() {
-    console.log(`Студент: ${this.fullName}.
- Возраст: ${this.age}.
- Средний балл: ${this.averageScore}.
- Итоговая оценка успеваемости: ${this.summary}.`);
   }
 }
 
-const anton = new Student("Anton", "Sharenko", 2002, [60, 80, 75, 43, 54, 87, 66, 90]);
-anton.fillAttendance();
-anton.consoleFullInfo;
-
-const igor = new Student("Igor", "Brom", 1993, [90, 80, 99, 85, 93, 82, 95, 100]);
-igor.fillAttendance();
-igor.consoleFullInfo;
-
-const anna = new Student("Anna", "Keysi", 1995, [100, 100, 100, 100, 100, 100, 100, 100]);
-anna.fillAttendance();
-anna.consoleFullInfo;
+const userTable = new Table(10, 10);
+userTable.createTable();
