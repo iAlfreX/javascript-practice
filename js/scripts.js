c183a55cfde0ff465f5a10e9547ed80ff3610f8a
Anton Sharenko <alpha.tosha@gmail.com>
2023-11-30 02:07:36 +0200

    homework-2 30.11.2023

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,27 @@
 "use strict";
 
-class DataValidation {
-  static checkForNumber(value) {
-    return typeof value === "number" && !isNaN(value);
-  }
+class Image {
+  getRandomImage() {
+    document.body.innerHTML = "";
 
-  static checkForInteger(value) {
-    return Number.isInteger(value);
-  }
-}
+    const randomNumber = Math.floor(Math.random() * 9) + 1;
+    const div = document.createElement("div");
+    div.style.cssText = "width: 500px; height: 500px; overflow: hidden;";
 
-class Table {
-  constructor(rows, cols) {
-    this.rows = DataValidation.checkForNumber(rows) && DataValidation.checkForInteger(rows) ? rows : 0;
-    this.cols = DataValidation.checkForNumber(cols) && DataValidation.checkForInteger(cols) ? cols : 0;
-  }
+    document.body.style.cssText = "margin: 0; height: 100vh; display: flex; justify-content: center; align-items: center;";
+    document.body.insertBefore(div, document.body.firstChild);
 
-  get amountOfRows() {
-    return this.rows;
-  }
+    const img = document.createElement("img");
+    img.style.cssText = "width: 100%; height: 100%;";
+    img.setAttribute("alt", "Картинка");
+    img.setAttribute("src", `../images/${randomNumber}.jpg`);
 
-  set amountOfRows(value) {
-    if (DataValidation.checkForNumber(value) && DataValidation.checkForInteger(value)) {
-      this.rows = value;
-    } else {
-      console.log(`${value} не является допустимым значением!`);
-    }
-  }
-
-  get amountOfCols() {
-    return this.cols;
-  }
-
-  set amountOfCols(value) {
-    if (DataValidation.checkForNumber(value) && DataValidation.checkForInteger(value)) {
-      this.cols = value;
-    } else {
-      console.log(`${value} не является допустимым значением!`);
-    }
-  }
-
-  createTable() {
-    const table = document.createElement("table");
-    let counter = 0;
-    table.style.cssText = "border-collapse: collapse; width: 50%; height: 50%;";
-    // table.style.borderCollapse = "collapse";
-    // table.style.width = "100%";
-    // table.style.border = "1px solid #000";
-    document.body.style.cssText = "margin: 0; display: flex; justify-content: center; align-items: center; height: 100vh;";
-    document.body.insertBefore(table, document.body.firstChild);
-
-    for (let rows = 1; rows <= this.amountOfRows; rows++) {
-      const tableRow = document.createElement("tr");
-      table.appendChild(tableRow);
-
-      for (let cols = 1; cols <= this.amountOfCols; cols++) {
-        const tableData = document.createElement("td");
-        tableData.style.cssText = "text-align: center; width: 10%; border: 1px solid #000;";
-        tableData.textContent = ++counter;
-        tableRow.appendChild(tableData);
-      }
-    }
+    div.appendChild(img);
   }
 }
 
-const userTable = new Table(10, 10);
-userTable.createTable();
+const myImage = new Image();
+myImage.getRandomImage();
+
+// const anotherImage = new Image();
+// anotherImage.getRandomImage();
